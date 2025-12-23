b4d1192a8da8addd820ffe130d7baea30c342644
정지민 <jeongjimin@jeongjimin-ui-MacBookPro.local>
2025-12-23 19:30:04 +0900

    준비하시고

--- getNextDir.js
+++ getNextDir.js
@@ -1,33 +1,67 @@
-function getNextDir() {
+const upward = [0, 0, 0, 0, 0];
+const downward = [0, 0, 0, 0, 0];
+function setElevatorState() {
   if (elevatorState == "UPWARD") {
     if (isThereUpAbove() == TRUE) {
-      return "UP";
+      elevatorState = "UPWARD";
     } else if (isThereDownAbove()) {
       elevatorState = "UPWARD";
-      return "UP";
     } else if (isThereDownBelow()) {
       elevatorState = "UPWARD";
-      return "UP";
     } else if (isThereUpBelow()) {
       elevatorState = "DOWNWARD";
-      return "DOWN";
     } else {
       elevatorState = "IDLE";
-      return "NONE";
     }
   } else if (elevatorState == "DOWNWARD") {
     if (isThereDownBelow()) {
       elevatorState = "DOWNWARD";
-      return "DOWN";
     } else if (isThereUpBelow()) {
       elevatorState = "DOWNWARD";
-      return "DOWN";
     } else if (isThereUpAbove()) {
       elevatorState = "UPWARD";
-      return "UP";
     } else if (isThereDownAbove()) {
       elevatorState = "UPWARD";
-      return "UP";
+    } else {
+      elevatorState = "IDLE";
     }
   }
+}
+
+async function moveByStep() {
+  setElevatorState();
+  if (elevatorState == "UPWARD") {
+    await moveUp();
+  } else if (elevatorState == "DOWNWARD") {
+    await moveDown();
+  } else if (elevatorState == "IDLE") {
+    console.log(elevatorState);
+    return;
+  }
+  moveByStep();
+}
+
+function isThereUpAbove() {
+  for (i = 4; i > curFloor; i--) {
+    if (upward[i]) return true;
+  }
+  return false;
+}
+function isThereDownAbove() {
+  for (i = 4; i > curFloor; i--) {
+    if (downward[i]) return true;
+  }
+  return false;
+}
+function isThereUpBelow() {
+  for (i = 0; i < curFloor; i++) {
+    if (upward[i]) return true;
+  }
+  return false;
+}
+function isThereDownBelow() {
+  for (i = 0; i < curFloor; i++) {
+    if (downward[i]) return true;
+  }
+  return false;
 }
